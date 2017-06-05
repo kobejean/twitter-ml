b17a1e69896e8a2226d1997e06f73fb6ed0b0cc2
kobejean <kobejean@me.com>
2017-06-05 17:03:27 -0600

    create_text.py makes data more sparse

--- src/create_text.py
+++ src/create_text.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 import random
 import os
+import re
 
 from context import tml
 from tml.collection.data_handler import DataHandler
@@ -20,16 +21,24 @@ dat_hand.display()
 
 batches = []
 batch_size = 2000
-for i, entry in enumerate(dat_hand.data):
+tweet_texts = set([])
+for entry in dat_hand.data:
+    # replace urls with generic url
+    text = entry.get("text","")
+    text = re.sub('https\:\/\/t\.co\/.{10}', 'https://t.co/XXXXXXXXXX', text, re.MULTILINE)
+    tweet_texts.add(text)
+
+for i, text in enumerate(tweet_texts):
     batch_num = i // batch_size
     if i % batch_size == 0:
         batches.append([])
-    batches[batch_num].append(entry)
+
+    batches[batch_num].append(text)
 
 for i, batch in enumerate(batches):
-    write_path = os.path.join(data_path, "SPACEX TEXT " + str(i+1) + ".txt")
+    write_path = os.path.join(data_path, "SPACEX TEXT " + str(i+1).zfill(2) + ".txt")
     write_file = open(write_path, 'w')
-    for entry in batch:
-        write_file.write(entry.get("text","")) # python will convert \n to os.linesep
+    for text in batch:
+        write_file.write(text+"\n") # python will convert \n to os.linesep
 
     write_file.close()
