063b776ede6cac1f37d1631de33403a82c16bb3a
kobejean <kobejean@me.com>
2017-06-16 13:40:57 -0600

    Added word stats

---
+++ src/word_count_stats.py
@@ -0,0 +1,20 @@
+import os
+from context import tml
+from tml.learning.word_stats import *
+from tml.utils.ansi import ANSI
+
+TEST_MODE = False
+
+
+file_name = input("FILE NAME OF TXT FILE:") if not TEST_MODE else "THE STREAM.csv"
+num_words = int(input("NUM WORDS:")) if not TEST_MODE else 10000
+# paths
+abs_path = os.path.abspath(os.path.dirname(__file__))
+data_path = os.path.join(abs_path, "data")
+read_path = os.path.join(data_path, file_name)
+
+word_counts = get_word_stats_from_file(read_path, num_words)
+print("WORD & COUNT")
+for word, count in word_counts:
+    word_text = ANSI.PURPLE + word.upper() + ": " + ANSI.ENDC
+    print(word_text + str(count))
