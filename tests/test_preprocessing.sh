f8802c1790e627ab46e3b01ecb0a768a7b07ab3d
kobejean <kobejean@me.com>
2017-07-15 17:24:18 -0600

    Travis CI fix attempt

--- tests/test_preprocessing.sh
+++ tests/test_preprocessing.sh
@@ -10,8 +10,17 @@
 
 echo "PREPROCESSING FOR CHARACTER PREDICTION..."
 python3 ../src/character_prediction/create_text.py data/THE\ STREAM.txt data/THE\ STREAM/
+local status1=$?
 
 echo "PREPROCESSING FOR WORD EMBEDDINGS..."
 python3 ../src/word_embeddings/create_data_package.py data/THE\ STREAM.txt data/THE\ STREAM/
+local status2=$?
 
 rm -r data/THE\ STREAM/
+
+if [status1 == 0 && status2 == 0];
+    then
+        exit 0
+    else
+        exit 1
+fi
