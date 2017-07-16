7cd9ca40786dd792e90dd0e4a13278887e07a440
kobejean <kobejean@me.com>
2017-07-16 08:06:07 -0600

    TravisCI fix attempt 8

--- tests/test_preprocessing.sh
+++ tests/test_preprocessing.sh
@@ -12,14 +12,14 @@ echo "PREPROCESSING FOR CHARACTER PREDICTION..."
 python3 ../src/character_prediction/create_text.py data/THE\ STREAM.txt data/THE\ STREAM/
 status1=$?
 
-# echo "PREPROCESSING FOR WORD EMBEDDINGS..."
-# python3 ../src/word_embeddings/create_data_package.py data/THE\ STREAM.txt data/THE\ STREAM/
-# status2=$?
+echo "PREPROCESSING FOR WORD EMBEDDINGS..."
+python3 ../src/word_embeddings/create_data_package.py data/THE\ STREAM.txt data/THE\ STREAM/
+status2=$?
 
 rm -r data/THE\ STREAM/
 
-# if [ $status1 -eq 0 ] && [ $status2 -eq 0 ]
-if [ $status1 -eq 0 ]
+if [ $status1 -eq 0 ] && [ $status2 -eq 0 ]
+# if [ $status1 -eq 0 ]
     then
         exit 0
     else
