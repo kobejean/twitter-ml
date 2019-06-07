bce9135fed7baba8dac4745085acd6e9da5147d9
Jean Flaherty <kobejean@me.com>
2019-06-06 20:17:27 -0600

    uses setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='tml',
+      version='0.0',
+      description='A python module for machine learning with twitter data.',
+      url='http://github.com/kobejean/twitter-ml',
+      packages=[
+          'tml',
+          'tml.collection',
+          'tml.learning.character_prediction',
+          'tml.learning.word_embeddings',
+          'tml.utils',
+      ],
+      install_requires=[
+          'tensorflow>=1.12.2',
+          'tweepy>=3.7.0',
+          'keras>=2.2.4',
+      ],
+      zip_safe=False)
